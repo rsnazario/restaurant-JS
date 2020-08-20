d99924c8031b70efd937f350e24553e3b198319b
rsnazario <rafael.s.nazario@gmail.com>
2020-08-20 14:32:12 -0300

    Removed DOM from logic-controller

--- src/index.js
+++ src/index.js
@@ -5,35 +5,7 @@ import { Contact } from './contactTab';
 import { About } from './aboutTab';
 
 const UIAssembler = (function (select, home, menu, contact, about) {
-  return {
-    setSelectTabs() {
-      select.createSelectTabsElements();
-    },
-
-    setHome() {
-      document.getElementById('home').classList.add('tabs-active');
-      home.createHomeElements();
-    },
-
-    setMenu() {
-      document.getElementById('menu').classList.add('tabs-active');
-      menu.createMenuElements();
-    },
-
-    setContact() {
-      document.getElementById('contact').classList.add('tabs-active');
-      contact.createContactElements();
-    },
-
-    setAbout() {
-      document.getElementById('about').classList.add('tabs-active');
-      about.createAboutElements();
-    },
-  };
-}(SelectTabs(), Home(), Menu(), Contact(), About()));
-
-const Controller = (function (Assembler) {
-  const deleteDisplayedScreen = function () {
+  function deleteDisplayedScreen() {
     const contentDiv = document.getElementById('content');
     if (contentDiv.lastChild.id !== 'select-tabs') {
       contentDiv.removeChild(contentDiv.lastChild);
@@ -42,43 +14,60 @@ const Controller = (function (Assembler) {
     }
   };
 
+  return {
+    setSelectTabs() {
+      select.createSelectTabsElements();
+    },
+
+    setHome() {
+      deleteDisplayedScreen();
+      document.getElementById('home').classList.add('tabs-active');
+      home.createHomeElements();
+    },
+
+    setMenu() {
+      deleteDisplayedScreen();
+      document.getElementById('menu').classList.add('tabs-active');
+      menu.createMenuElements();
+    },
+
+    setContact() {
+      deleteDisplayedScreen();
+      document.getElementById('contact').classList.add('tabs-active');
+      contact.createContactElements();
+    },
+
+    setAbout() {
+      deleteDisplayedScreen();
+      document.getElementById('about').classList.add('tabs-active');
+      about.createAboutElements();
+    },
+  };
+}(SelectTabs(), Home(), Menu(), Contact(), About()));
+
+const Controller = (function (Assembler) {
   const linkClicked = function (event) {
     if (event.target.id === 'link-menu') {
-      deleteDisplayedScreen();
       Assembler.setMenu();
     } else if (event.target.id === 'link-contact') {
-      deleteDisplayedScreen();
       Assembler.setContact();
     } else if (event.target.id === 'link-about') {
-      deleteDisplayedScreen();
       Assembler.setAbout();
     }
   };
 
   const setupEventListeners = function () {
     document.getElementById('home').addEventListener('click', () => {
-      // 1 - Delete Current Displayed Screen
-      deleteDisplayedScreen();
-      // 2 - Display Home Tab
       Assembler.setHome();
     });
     document.getElementById('content').addEventListener('click', linkClicked);
     document.getElementById('menu').addEventListener('click', () => {
-      // 1 - Delete Current Displayed Screen
-      deleteDisplayedScreen();
-      // 2 - Display Menu Tab
       Assembler.setMenu();
     });
     document.getElementById('contact').addEventListener('click', () => {
-      // 1 - Delete Current Displayed Screen
-      deleteDisplayedScreen();
-      // 2 - Display Contact Tab
       Assembler.setContact();
     });
     document.getElementById('about').addEventListener('click', () => {
-      // 1 - Delete Current Displayed Screen
-      deleteDisplayedScreen();
-      // 2 - Display About Tab
       Assembler.setAbout();
     });
   };
